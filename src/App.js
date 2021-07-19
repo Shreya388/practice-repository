bdbeb403d21c7700c81f4f1351d5e085f237d683
Shreya388 <xjshreya39@gmail.com>
2021-07-19 22:50:53 +0530

    working on props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from "./components/Header";
 
+
+
 function App() {
   return (
     <div className="App">
-      <Header name = "Bucchi Jha"/>
-      <Header name = "dhinchak" />
+      <Header name = "Bucchi Jha" year="2001" />
+      <Header name = "dhinchak" year="2002"/>
+      
     </div>
-    
     
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 function Header(props) {
     return <div>
-        <h1>{props.name}</h1>
+        <h1>{props.year}, {props.name}</h1>
+        <Button variant="primary">apple</Button>
     </div>;
 }
 
